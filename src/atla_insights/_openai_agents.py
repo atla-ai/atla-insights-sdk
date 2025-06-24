df1a6416af96e68710ec77c3c700fc36ae326dba
Mathias Leys <mathias@atla-ai.com>
2025-06-24 17:47:34 +0100

    Log OpenAI agent tool invocations

--- src/atla_insights/_openai_agents.py
+++ src/atla_insights/_openai_agents.py
@@ -2,9 +2,12 @@
 
 from typing import Any
 
+from wrapt import wrap_function_wrapper
+
 try:
     from agents import set_trace_processors
     from openinference.instrumentation.openai_agents import OpenAIAgentsInstrumentor
+    from openinference.semconv.trace import SpanAttributes
 except ImportError as e:
     raise ImportError(
         "OpenAI agents instrumentation needs to be installed. "
@@ -12,8 +15,32 @@ except ImportError as e:
     ) from e
 
 
+def _get_attributes_from_function_span_data(
+    wrapped: Any, instance: Any, args: Any, kwargs: Any
+) -> Any:
+    """Wrap the _get_attributes_from_function_span_data function to add tool params."""
+    yield from wrapped(*args, **kwargs)
+
+    obj = args[0]
+    if obj.input:
+        yield SpanAttributes.TOOL_PARAMETERS, obj.input
+    else:
+        yield SpanAttributes.TOOL_PARAMETERS, "{}"
+
+    if obj.output is None:
+        yield SpanAttributes.OUTPUT_VALUE, "{}"
+
+
 class AtlaOpenAIAgentsInstrumentor(OpenAIAgentsInstrumentor):
     """Atla OpenAI Agents SDK instrumentor class."""
+
+    def _instrument(self, **kwargs: Any) -> None:
+        wrap_function_wrapper(
+            "openinference.instrumentation.openai_agents._processor",
+            "_get_attributes_from_function_span_data",
+            _get_attributes_from_function_span_data,
+        )
+        super()._instrument(**kwargs)
 
     def _uninstrument(self, **kwargs: Any) -> None:
         set_trace_processors([])
