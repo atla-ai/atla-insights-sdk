a38f3167fcadcfd8d5f14b13407e71015a304097
Mathias Leys <mathias@atla-ai.com>
2025-06-24 16:47:19 +0100

    Wrap langchain openinference import

--- src/atla_insights/_langchain.py
+++ src/atla_insights/_langchain.py
@@ -3,10 +3,17 @@
 import ast
 from typing import Any
 
-from openinference.instrumentation import safe_json_dumps
-from openinference.instrumentation.langchain import LangChainInstrumentor
-from openinference.semconv.trace import SpanAttributes
 from wrapt import wrap_function_wrapper
+
+try:
+    from openinference.instrumentation import safe_json_dumps
+    from openinference.instrumentation.langchain import LangChainInstrumentor
+    from openinference.semconv.trace import SpanAttributes
+except ImportError as e:
+    raise ImportError(
+        "LangChain instrumentation needs to be installed. "
+        "Please install it via `pip install atla-insights[langchain]`."
+    ) from e
 
 
 def _tools(wrapped: Any, instance: Any, args: Any, kwargs: Any) -> Any:
