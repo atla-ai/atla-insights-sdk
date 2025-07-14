be651992576c977190004733e75f2bf164b72adb
Toby <tobydrane@gmail.com>
2025-07-14 17:55:15 +0100

    Fix precommits

--- src/atla_insights/llm_providers/bedrock.py
+++ src/atla_insights/llm_providers/bedrock.py
@@ -1,9 +1,25 @@
+"""Bedrock LLM provider instrumentation."""
+
 from typing import ContextManager
 
 from atla_insights.main import ATLA_INSTANCE
 
 
 def instrument_bedrock() -> ContextManager[None]:
+    """Instrument the Bedrock LLM provider.
+
+    This function creates a context manager that instruments the Bedrock LLM provider,
+    within its context.
+
+    ```py
+    from atla_insights import instrument_bedrock
+
+    with instrument_bedrock():
+        # My Bedrock code here
+    ```
+
+    :return (ContextManager[None]): A context manager that instruments Bedrock.
+    """
     try:
         from openinference.instrumentation.bedrock import BedrockInstrumentor
     except ImportError as e:
@@ -21,4 +37,5 @@ def instrument_bedrock() -> ContextManager[None]:
 
 
 def uninstrument_bedrock() -> None:
+    """Uninstrument the Bedrock LLM provider."""
     return ATLA_INSTANCE.uninstrument_service("bedrock")
