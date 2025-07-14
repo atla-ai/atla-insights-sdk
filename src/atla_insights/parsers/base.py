8a031202dc4387ec3737fac35ad4c37722e0ed1a
Mathias Leys <mathias@atla-ai.com>
2025-07-14 17:40:23 +0100

    Add docs

--- src/atla_insights/parsers/base.py
+++ src/atla_insights/parsers/base.py
@@ -3,18 +3,25 @@
 from abc import ABC, abstractmethod
 from typing import Any, Generator
 
+from atla_insights.constants import SUPPORTED_LLM_FORMAT
+
 
 class BaseParser(ABC):
     """Base parser for the atla_insights package."""
 
-    name: str
+    name: SUPPORTED_LLM_FORMAT
 
     @abstractmethod
     def parse_request_body(
         self,
         request: dict[str, Any],
     ) -> Generator[tuple[str, Any], None, None]:
-        """Parse the raw request body."""
+        """Parse the raw request body sent to a certain LLM provider.
+
+        :param request (dict[str, Any]): The raw request body.
+        :return (Generator[tuple[str, Any], None, None]): A generator of tuples that
+            represent the span attribute key-value pairs of the request body.
+        """
         ...
 
     @abstractmethod
@@ -22,5 +29,10 @@ class BaseParser(ABC):
         self,
         response: dict[str, Any],
     ) -> Generator[tuple[str, Any], None, None]:
-        """Parse the raw response body."""
+        """Parse the raw response body received from a certain LLM provider.
+
+        :param response (dict[str, Any]): The raw response body.
+        :return (Generator[tuple[str, Any], None, None]): A generator of tuples that
+            represent the span attribute key-value pairs of the response body.
+        """
         ...
