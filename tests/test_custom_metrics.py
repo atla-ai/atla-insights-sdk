029d3445ce01a29e9220476458a28d885afb7588
Mathias Leys <mathias@atla-ai.com>
2025-07-29 10:58:50 +0100

    Add getter / setter test cases

--- tests/test_custom_metrics.py
+++ tests/test_custom_metrics.py
@@ -60,3 +60,38 @@ class TestCustomMetrics(BaseLocalOtel):
 
         assert span_2.attributes is not None
         assert span_2.attributes.get(CUSTOM_METRICS_MARK) is None
+
+    def test_get_set_custom_metrics(self) -> None:
+        """Test that the custom metrics is set and retrieved correctly."""
+        from atla_insights import instrument
+        from atla_insights.constants import CUSTOM_METRICS_MARK
+        from atla_insights.custom_metrics import (
+            CustomMetric,
+            get_custom_metrics,
+            set_custom_metrics,
+        )
+
+        @instrument()
+        def test_function():
+            metrics_1: dict[str, CustomMetric] = {
+                "test": {"data_type": "boolean", "value": False}
+            }
+            set_custom_metrics(metrics_1)
+            assert get_custom_metrics() == metrics_1
+
+            metrics_2: dict[str, CustomMetric] = {
+                "test": {"data_type": "boolean", "value": True}
+            }
+            set_custom_metrics(metrics_2)
+            assert get_custom_metrics() == metrics_2
+
+            return "test result"
+
+        test_function()
+        spans = self.get_finished_spans()
+
+        assert len(spans) == 1
+        [span] = spans
+
+        assert span.attributes is not None
+        assert span.attributes.get(CUSTOM_METRICS_MARK) is not None
